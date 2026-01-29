accca0cc653230acae962f77d3d2f4ec4b6616ec
mariaragaodev <marianaaragao.dev@gmail.com>
2026-01-29 20:42:03 +0000

    declarei as variaveis

---
+++ super_trunfo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+    //Estado 1
+    char estado1;
+    char codigo1 [4];
+    char cidade1 [35];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTur1;
+
+    //Estado 2
+    char estado2;
+    char codigo2 [4];
+    char cidade2 [35];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTur2;
+
+}
