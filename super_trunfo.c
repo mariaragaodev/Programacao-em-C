7e047761d2fd25edb559020edd4d85f7f618fa7a
mariaragaodev <marianaaragao.dev@gmail.com>
2026-01-29 21:01:07 +0000

    finalizei a entrada de dados

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,27 +1,58 @@
 #include <stdio.h>
 
 int main (){
-    //Estado 1
+    //Carta 1
     char estado1;
     char codigo1 [4];
-    char cidade1 [35];
+    char cidade1 [45];
     int populacao1;
     float area1;
     float pib1;
     int pontosTur1;
 
-    //Estado 2
+    //Carta 2
     char estado2;
     char codigo2 [4];
-    char cidade2 [35];
+    char cidade2 [45];
     int populacao2;
     float area2;
     float pib2;
     int pontosTur2;
 
+    //Entrada de dados carta 1
     printf("--- INSIRA AS INFORMAÇÕES DA CARTA 1 ---\n");
     printf("Estado (A a H): ");
-    scanf(" %c", estado1);
+    scanf(" %c", &estado1);
+    printf("Código da carta (A01, B03): ");
+    scanf(" %s", codigo1);
+    printf("Nome da cidade: ");
+    scanf(" %s", cidade1);
+    printf("População: ");
+    scanf(" %d", &populacao1);
+    printf("Área: ");
+    scanf(" %f", &area1);
+    printf("PIB: ");
+    scanf(" %f", &pib1);
+    printf("Número de Pontos Turísticos: ");
+    scanf(" %d", &pontosTur1);
     
+    //Entrada de dados carta 2
+    printf("--- INSIRA AS INFORMAÇÕES DA CARTA 2 ---\n");
+    printf("Estado (A a H): ");
+    scanf(" %c", &estado2);
+    printf("Código da carta (A01, B03): ");
+    scanf(" %s", codigo2);
+    printf("Nome da cidade: ");
+    scanf(" %s", cidade2);
+    printf("População: ");
+    scanf(" %d", &populacao2);
+    printf("Área: ");
+    scanf(" %f", &area2);
+    printf("PIB: ");
+    scanf(" %f", &pib2);
+    printf("Número de Pontos Turísticos: ");
+    scanf(" %d", &pontosTur2);
+    
+
 
 }
